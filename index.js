f92c045861b9419f187d477a4efd5f43eb8c4d92
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-19 17:16:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", main );
 
 function main(){
+    
+    const befizetendo=document.querySelector("#befizetendo");
+    
+
     document.querySelector("#get-new-osszeg").onclick=ujOsszeg
     document.querySelector("#befizetes").onclick=visszajaroSzamol
-
-    const befizetendo=document.querySelector("#befizetendo")
 }
 
 function ujOsszeg(){
-const num=Math.floor(Math.random()*99900+100);
+const num=Math.floor(Math.random()*99900+101);
 befizetendo.innerHTML=num +" Ft";
 }
 
 function visszajaroSzamol(){
-    console.log("Megy");
+    
+    let befizetett=document.querySelector("#befizetett").value;
+    
 }
