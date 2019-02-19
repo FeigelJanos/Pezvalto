6abbab14617d51d6e7d7e08ee694ece49d0add64
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-19 16:49:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-document.addEventListener("DOMContentLoaded", ()=>{document.querySelector("#get-new-osszeg").onclick=ujOsszeg
-                                                  });
+document.addEventListener("DOMContentLoaded", main );
+
+function main(){
+    document.querySelector("#get-new-osszeg").onclick=ujOsszeg
+    document.querySelector("#befizetes").onclick=visszajaroSzamol
+
+    const befizetendo=document.querySelector("#befizetendo")
+}
 
 function ujOsszeg(){
-console.log("Megy");
+const num=Math.floor(Math.random()*99900+100);
+befizetendo.innerHTML=num +" Ft";
+}
+
+function visszajaroSzamol(){
+    console.log("Megy");
 }
