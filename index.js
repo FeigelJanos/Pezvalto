30e1122b841e9a6239508a27873df65d947d15a5
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-21 12:08:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", main ); 
- /*/As long as the CSS and HTML is not fully loaded, javascript content is on hold, after it starts main.
+ /*As long as the CSS and HTML is not fully loaded, javascript content is on hold, after it starts main.*/
 
 
 /*Main function starts here*/
